3fa06d1207232668ecc0398690072af7e535038b
Muhammad Waheed Iqbal <tesla.dxb@gmail.com>
2023-05-26 09:15:54 +0400

    app ready to share

--- data/page-data.jsx
+++ data/page-data.jsx
@@ -1,23 +1,22 @@
 export const personalData = {
-	name: "Joe Doe",
-	role: "Software Engineer",
+	name: "M. Waheed Iqbal",
+	role: "Blockchain/ Web 3.0 Developer, Treasurer, Debt Collector",
 	education: [
-		"Bachelor of Science in Computer Science, University of Oxford(2015-2019)",
-		"Master of Science in Artificial Intelligence, Imperial College London (2019-2021)",
+		"Bachelor of Commerce, University of the Punjab (2000)",
 	],
 	contactLinks: [
-		"mailto:emailto:time2code@gmail.com",
-		"https://twitter.com/time_tocode",
-		"https://linkedin.com/aliciarodriguezLi",
-		"https://medium.com/@time2code.ali",
+		"mailto:emailto:tesla.dxb@gmail.com",
+		"https://twitter.com/WaheedConnects",
+		"https://linkedin.com/in/waheedconnects",
+		"https://github.com/MWaheedIqbal",
 	],
 };
 
 export const aboutMe = {
 	title: "About Me",
 	body: [
-		"As a highly motivated and driven individual with a passion for making a positive impact, I have a unique blend of technical and interpersonal skills, thanks to my background in computer science. I am able to tackle complex problems with ease, and I never lose sight of my ultimate goal, even when facing challenges.",
-		"When I'm not working, I love hiking, reading, and spending time with my friends and family. I believe that my determination, can-do attitude, and hard work make me a true asset to any team.",
+		"As a highly motivated and driven individual with a passion for making a positive impact and purposeful living, I have a unique blend of technical and interpersonal skills, thanks to my diverse experience. I am able to tackle problems with ease, and I never lose sight of my ultimate goal, even when facing challenges.",
+		"When I'm not working, I love driving, reading, and spending time with my family & friends. I believe that my determination, can-do attitude, and hard work make me a true asset to any team.",
 	],
 };
 
@@ -44,22 +43,22 @@ export const professionalData = {
 	title: "Professional Experience",
 	experiences: [
 		{
-			role: "Data Scientist, Acme Inc. (2021-Present)",
+			role: "Administrative Director (Mar 2023-Present)",
 			description:
 				"Design and implement machine learning models to analyze large datasets and drive business decisions. Collaborate with cross-functional teams to develop and launch new products and features. Provide technical guidance and mentorship to junior data scientists. Conduct regular presentations to stakeholders on the findings and insights generated from data analysis.",
 			current: true,
 		},
 
 		{
-			role: "Machine Learning Engineer, XYZ Corp. (2019-2021)",
+			role: "Business Owner 'BagsOnClick' (May 2020-Present)",
 			description:
-				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
-			current: false,
+				"Designed and implemented e-commerce webstore for eco-friendly and sustainable products. Exploring to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
+			current: true,
 		},
 		{
-			role: "Machine Learning Engineer, ABX Corp. (2020-2021)",
+			role: "Finance Manager. (2015)",
 			description:
-				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
+				"Performed Accounting and Finance Management role in MNCs in United Arab Emirates",
 			current: false,
 		},
 	],
